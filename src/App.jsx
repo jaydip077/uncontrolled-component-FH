ad5473936828c47acff0af439794256312eee21b
jaydip077 <ahirjaydip2021@gmail.com>
2026-05-08 15:51:20 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
   };
 
   const showToastMessage = () => {
-    toast.success("Success Notification !", {
+    toast.success("Appoitment Booked Succesfully !", {
       position: "top-right",
     });
   };
